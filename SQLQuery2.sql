2e7865fe5acf4a0805b44ebaf64ab7bd1b17c3f2
muhammed diri <muhmdaley@gmail.com>
2021-01-28 00:06:26 +0300

    Add project files.

---
+++ SQLQuery2.sql
@@ -0,0 +1,34 @@
+﻿--Select
+--ANSII
+Select ContactName Adi, CompanyName SirketAdi,City Sehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+--case insensitive
+Select * from Products where categoryId=1 or categoryId=3
+
+Select * from Products where categoryId=1 and UnitPrice>=10
+
+select * from Products where categoryId=1 order by UnitPrice desc  --ascending(artan sırala) --descending
+
+select count(*) Adet from Products where CategoryID =2
+
+select categoryID,count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+from products inner join Categories
+on Products.CategoryID = Categories.CategoryID
+where Products.UnitPrice>10
+
+--DTO Data Transformation object
+
+Select * from Products p inner join [Order Details] od
+on p.ProductID = od.ProductID
+inner join Orders o 
+on o.OrderID = od.OrderID
+
+select * from Customers c left join Orders o
+on C.CustomerID = o.CustomerID
+where o.CustomerID is null 
+--sql veri sorgulama dms
